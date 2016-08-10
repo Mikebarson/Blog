0271299f63be32988bfd06008983bc8fe4e6abbe
Mikebarson <ronmichaeloffice@gmail.com>
2016-08-10 15:24:20 +0800

    Added login and logout for user - save as draft - crud fr posts

--- blog/views.py
+++ blog/views.py
@@ -2,22 +2,26 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import *
 from .forms import * 
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+
 # Create your views here.
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'blog/post_list.html', {'posts' : posts})
 
+@login_required
 def post_detail(request,pk):
 	post = get_object_or_404(Post, pk=pk)
 	return render(request, 'blog/post_detail.html', {'post': post})
 
+@login_required
 def post_new(request):
 	if request.method == "POST":
 		form = PostForm(request.POST)
 		if form.is_valid():
 			post  = form.save(commit=False)
 			post.author = request.user
-			post.published_date = timezone.now()
 			post.save()
 			return redirect('post_detail', pk=post.pk)
 
@@ -26,6 +30,7 @@ def post_new(request):
 		form = PostForm()
 	return render(request,  'blog/post_edit.html', {'form' : form})
 
+@login_required
 def post_edit(request, pk):
 	post  = get_object_or_404(Post, pk=pk)
 	if request.method == "POST":
@@ -34,9 +39,42 @@ def post_edit(request, pk):
 		if form.is_valid():
 			post = form.save(commit=False)
 			post.author = request.user
-			post.published_date = timezone.now()
 			post.save()
 			return redirect('post_detail', pk=post.pk)
 	else:
 		form = PostForm(instance=post)
 	return render(request, 'blog/post_edit.html', {'form': form})
+
+@login_required
+def post_draft_list(request):
+	posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+	return render(request, 'blog/post_draft_list.html', {'posts':posts})
+
+@login_required
+def post_publish(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	post.publish()
+	return redirect('post_detail', pk=pk)
+
+@login_required
+def post_delete(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	post.delete()
+	return redirect('post_list')
+
+# def login(request):
+# 	if request.method == "POST":
+# 		username = request.POST['username']
+# 		password = request.POST['password']
+# 		user = authenticate(username=username, password=password)
+# 		if user is not None:
+# 			if user.is_active:
+# 				login(request, user)
+# 				redirect('post_list')
+# 		else:
+# 			return ValueError('User does not exist')
+# 	return render(request, 'registration/login.html')
+
+# def logout(request):
+# 	logout(request)
+# 	return redirect('registration/login.html')
--- michael_blog/urls.py
+++ michael_blog/urls.py
@@ -1,11 +1,16 @@
 from django.conf.urls import include, url
+from django.contrib.auth import views
+
 from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'michael_blog.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^accounts/login/$', views.login, name='login'),
+    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page' : '/'}),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('blog.urls')),
 ]
